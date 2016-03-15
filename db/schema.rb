050e779e883c37382b6f5d65674cda1d198d52be
Marcelo J. Both <marcelo.both@gmail.com>
2016-03-14 21:46:55 -0300

    Added accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907031755) do
+ActiveRecord::Schema.define(version: 20160315004029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.integer  "account"
