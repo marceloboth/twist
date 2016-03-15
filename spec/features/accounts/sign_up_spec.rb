0d9a6fc1456adfe6bb0f37fec416418cbf2c678a
Marcelo J. Both <marcelo.both@gmail.com>
2016-03-14 22:11:09 -0300

    Accounts are now linked to owners

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -4,8 +4,16 @@ feature "Accounts" do
   scenario "creating an account" do
     visit root_path
     click_link "Create a new account"
+
     fill_in "Name", with: "Test"
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
     click_button "Create Account"
+
+    expect(page).to have_content("Signed in as test@example.com")
+
     within(".flash_notice") do
       success_message = "Your account has been successfully created."
       expect(page).to have_content(success_message)
